757b3d489e266e1f08489fa4055250332dbee5e5
Mann Marpaung <mannmarpaung@gmail.com>
2024-08-26 13:08:14 +0700

    3

--- lib/ui/login_screen.dart
+++ lib/ui/login_screen.dart
@@ -105,7 +105,7 @@ class _LoginScreenState extends State<LoginScreen> {
                         } else {
                           isSignedIn = true;
                         }
-                            // 2
+                            // 3
                         if (passwordController.text == null) {
                           ScaffoldMessenger.of(context).showSnackBar(
                             SnackBar(
